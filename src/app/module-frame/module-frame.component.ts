79aa669770f9fe6a11534f7242db60373493ab9a
Steffen Buhr <steffen.buhr@isb-ik.de>
2024-02-15 19:42:26 +0100

    Add fonts and navigation links to module-frame component

--- src/app/module-frame/module-frame.component.ts
+++ src/app/module-frame/module-frame.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+
+interface Page {
+  inMenu: boolean;
+  name: string;
+  link: string;
+  shortLink: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-module-frame',
@@ -10,13 +18,90 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './module-frame.component.scss',
 })
 export class ModuleFrameComponent {
-  popupMenuVisible = false;
+  pages = [
+    {
+      inMenu: false,
+      name: 'Dashboard',
+      link: 'Zum Dashboard',
+      shortLink: 'Dashboard',
+      route: '/dashboard',
+    },
+    {
+      inMenu: true,
+      name: 'Kompetenzatlas',
+      link: 'Zum Kompetenzatlas',
+      shortLink: 'Atlas',
+      route: '/competence-atlas',
+    },
+    {
+      inMenu: true,
+      name: 'Teamrollen',
+      link: 'Zu den Teamrollen',
+      shortLink: 'Teamrollen',
+      route: '/team-roles',
+    },
+    {
+      inMenu: true,
+      name: 'Soll-/Ist-Vergleich',
+      link: 'Zum Soll-/Ist-Vergleich',
+      shortLink: 'Soll-/Ist',
+      route: '/target-actual-comparison',
+    },
+    {
+      inMenu: true,
+      name: 'Stärken',
+      link: 'Zu den Stärken',
+      shortLink: 'Stärken',
+      route: '/strengths',
+    },
+    {
+      inMenu: true,
+      name: 'Übertreibungsvermutungen',
+      link: 'Zu den Übertreibungsvermutungen',
+      shortLink: 'Übertreibungen',
+      route: '/exaggerations',
+    },
+    {
+      inMenu: true,
+      name: 'Kompetenzentwicklung',
+      link: 'Zur Kompetenzentwicklung',
+      shortLink: 'Entwicklung',
+      route: '/competence-development',
+    },
+    {
+      inMenu: false,
+      name: 'Dashboard',
+      link: 'Zum Dashboard',
+      shortLink: 'Dashboard',
+      route: '/dashboard',
+    },
+  ];
+  title = signal('Kompetenzatlas');
+  previousPage = signal<Page>(this.pages[0]);
+  nextPage = signal<Page>(this.pages[2]);
 
-  showPopupMenu() {
-    this.popupMenuVisible = true;
+  gotoPage(page: Page) {
+    const current = this.pages.indexOf(page);
+    this.previousPage.set(this.pages[current - 1]);
+    this.title.set(page.name);
+    this.nextPage.set(this.pages[current + 1]);
   }
 
-  togglePopupMenu() {
-    this.popupMenuVisible = !this.popupMenuVisible;
+  gotoNextPage() {
+    const current = this.pages.findIndex((page) => page.name === this.title());
+    if (current < this.pages.length - 2) {
+      this.previousPage.set(this.pages[current]);
+      this.title.set(this.pages[current + 1].name);
+      this.nextPage.set(this.pages[current + 2]);
+    }
+  }
+
+  gotoPreviousPage() {
+    const current = this.pages.findIndex((page) => page.name === this.title());
+    if (current > 1) {
+      this.previousPage.set(this.pages[current - 2]);
+      this.title.set(this.pages[current - 1].name);
+      this.nextPage.set(this.pages[current]);
+    }
   }
 }
